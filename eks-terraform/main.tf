2d10baf3e90befaa64657a4ed10e85ea0fe6a748
Benhaya Messaoud Mohammed <85128270+benhayamohammedmessaoud5@users.noreply.github.com>
2026-01-07 02:18:45 +0100

    Remove OIDC provider due to AWS Academy restrictions

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -100,18 +100,3 @@ resource "aws_eks_node_group" "node-grp" {
 }
 
 # ----------------------------
-# OIDC Provider for ServiceAccount IAM Roles
-# ----------------------------
-data "aws_eks_cluster" "eks_oidc" {
-  name = aws_eks_cluster.eks.name
-}
-
-data "tls_certificate" "oidc_thumbprint" {
-  url = data.aws_eks_cluster.eks_oidc.identity[0].oidc[0].issuer
-}
-
-resource "aws_iam_openid_connect_provider" "eks_oidc" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.oidc_thumbprint.certificates[0].sha1_fingerprint]
-  url             = data.aws_eks_cluster.eks_oidc.identity[0].oidc[0].issuer
-}
